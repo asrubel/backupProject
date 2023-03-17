c6d3fe44889ab5a55d4617d2fb8274d462575fd4
aleksey <o.rubel@khai.edu>
2023-03-17 16:24:47 +0200

    Simple backup

--- backup.sh
+++ backup.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-#export AWS_ACCESS_KEY_ID="[....]"
-#export AWS_SECRET_ACCESS_KEY="[....]"
+TIMESTAMP=$(date +%d-%m-%Y"_"%H_%M_%S)
 
-DEST=boto3+s3://my-backup-bucket/
-GPG_KEY="45DBFFF2"
-VERBOSE=""
-#VERBOSE="-v8"
+docker exec -t postgres pg_dump -c -U postgres > /home/ubuntu/backups/dump_$TIMESTAMP.sql
+echo "Postgres dumped $TIMESTAMP" >> backup.log
 
-# Compression
-COMPRESSION=""
-#COMPRESSION="--gpg-options='--compress-algo=bzip2 --bzip2-compress-level=9'"
-
-INCLUDES="--include /home/ubuntu/backups"
-
-duplicity remove-older-than 2M ${DEST} --force
-# duplicity ${VERBOSE} ${COMPRESSION} --s3-use-ia --encrypt-key ${GPG_KEY} --full-if-older-than 1M ${INCLUDES} --exclude '**' / ${DEST}
-duplicity ${VERBOSE} ${COMPRESSION} --s3-use-ia --full-if-older-than 1D ${INCLUDES} --exclude '**' / ${DEST}
+aws s3 cp /home/ubuntu/backups/dump_$TIMESTAMP.sql s3://mybucket-for-college/backups/postgres_backup.sql
+echo "Dump loaded to AWS S3 $TIMESTAMP" >> backup.log
